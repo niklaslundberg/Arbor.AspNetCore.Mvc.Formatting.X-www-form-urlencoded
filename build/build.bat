acc57888ebc2894c9e1cfe44a2235c4b55803344
Niklas Lundberg <niklas.lundberg@telenor.se>
2019-11-05 19:31:24 +0100

    Started new NuGet packaging.

--- build/build.bat
+++ build/build.bat
@@ -1,23 +1,22 @@
 @ECHO OFF
-SET Arbor.X.Build.Bootstrapper.AllowPrerelease=true
-SET Arbor.X.Tools.External.MSpec.Enabled=true
-SET Arbor.X.MSBuild.NuGetRestore.Enabled=true
-SET Arbor.X.NuGet.Package.Artifacts.CreateOnAnyBranchEnabled=false
-SET Arbor.X.NuGet.Package.CreateNuGetWebPackages.Enabled=true
+SET Arbor.Build.Build.Bootstrapper.AllowPrerelease=true
+SET Arbor.Build.Tools.External.MSpec.Enabled=true
+SET Arbor.Build.MSBuild.NuGetRestore.Enabled=true
+SET Arbor.Build.NuGet.Package.Artifacts.CreateOnAnyBranchEnabled=false
+SET Arbor.Build.NuGet.Package.CreateNuGetWebPackages.Enabled=true
 CALL dotnet arbor-build
 
 REM Restore variables to default
 
-SET Arbor.X.Build.Bootstrapper.AllowPrerelease=
-REM SET Arbor.X.Vcs.Branch.Name=
-SET Arbor.X.Tools.External.MSpec.Enabled=
-SET Arbor.X.NuGet.Package.Artifacts.Suffix=
-SET Arbor.X.NuGet.Package.Artifacts.BuildNumber.Enabled=
-SET Arbor.X.Log.Level=
-SET Arbor.X.NuGetPackageVersion=
-SET Arbor.X.Vcs.Branch.Name.Version.OverrideEnabled=
-SET Arbor.X.VariableOverrideEnabled=
-SET Arbor.X.Artifacts.CleanupBeforeBuildEnabled=
-SET Arbor.X.Build.NetAssembly.Configuration=
+REM SET Arbor.Build.Vcs.Branch.Name=
+SET Arbor.Build.Tools.External.MSpec.Enabled=
+SET Arbor.Build.NuGet.Package.Artifacts.Suffix=
+SET Arbor.Build.NuGet.Package.Artifacts.BuildNumber.Enabled=
+SET Arbor.Build.Log.Level=
+SET Arbor.Build.NuGetPackageVersion=
+SET Arbor.Build.Vcs.Branch.Name.Version.OverrideEnabled=
+SET Arbor.Build.VariableOverrideEnabled=
+SET Arbor.Build.Artifacts.CleanupBeforeBuildEnabled=
+SET Arbor.Build.Build.NetAssembly.Configuration=
 
 EXIT /B %ERRORLEVEL%
